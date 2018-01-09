ca78e57413d8f966ec79ee2150df64bd47f97855
陈博 <chenbo@intra.nsfocus.com>
2018-01-09 21:43:40 +0800

    use k near to check

--- core/ocr/baiduocr.py
+++ core/ocr/baiduocr.py
@@ -9,7 +9,7 @@
 from aip import AipOcr
 
 
-def get_text_from_image(image_data, app_id, app_key, app_secret, api_version=0, timeout=3):
+def get_text_from_image(image_data, app_id, app_key, app_secret, timeout=3):
     """
     Get image text use baidu ocr
 
@@ -17,7 +17,6 @@ def get_text_from_image(image_data, app_id, app_key, app_secret, api_version=0, 
     :param app_id:
     :param app_key:
     :param app_secret:
-    :param api_version:
     :param timeout:
     :return:
     """
@@ -26,13 +25,12 @@ def get_text_from_image(image_data, app_id, app_key, app_secret, api_version=0, 
 
     options = {}
     options["language_type"] = "CHN_ENG"
+    options["detect_direction"] = "true"
+    options["detect_language"] = "true"
+    options["probability"] = "true"
 
-    if api_version == 1:
-        result = client.basicAccurate(image_data, options)
-    else:
-        result = client.basicGeneral(image_data, options)
-
+    result = client.basicGeneral(image_data, options)
     if "error_code" in result:
         print("baidu api error: ", result["error_msg"])
         return ""
-    return "".join([words["words"] for words in result["words_result"]])
+    return [words["words"] for words in result["words_result"]]
